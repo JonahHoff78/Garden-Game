ec402d8f3fc2658a281e9bb6b57fb6b5d3662793
Jonah Hoff <jojoyo98@icloud.com>
2026-01-15 22:53:58 -0700

    Camera-Grid a3

--- main.gd
+++ main.gd
@@ -6,4 +6,4 @@ func _physics_process(_delta):
 	var playerMapCoord = local_to_map(player.global_position)
 	var playerDir = player.direction
 	var playerMapsCoord = playerMapCoord + Vector2i(playerDir) / 32
-	grid_helper.global_position = playerMapCoord * 16
+	grid_helper.global_position = map_to_local(playerMapsCoord)
