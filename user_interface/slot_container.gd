343ba6b437b0ab83dbc448a62a640580db209d4f
Jonah Hoff <jojoyo98@icloud.com>
2026-01-30 15:27:36 -0700

    Inventory 2 a3

--- user_interface/slot_container.gd
+++ user_interface/slot_container.gd
@@ -10,6 +10,10 @@ var seedDataResource: SeedData
 
 
 func _on_texture_button_button_down() -> void:
-	if seedDataResource.seed_left(): pass
+	if seedDataResource.seed_left(): Global.emit_signal("seed_changed", seedDataResource)
+	emit_signal("slot_selected", position)
+	
+func update_quantity() -> void:
+	itemInfo.set_label(seedDataResource.get_quantity())
 	
 	
