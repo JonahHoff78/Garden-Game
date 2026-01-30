1a69d324b665a566551178a869fbe5f498b5de18
Jonah Hoff <jojoyo98@icloud.com>
2026-01-30 16:14:37 -0700

    Inventory 2 a7

--- user_interface/inventory.gd
+++ user_interface/inventory.gd
@@ -9,4 +9,20 @@ extends PanelContainer
 
 func _ready():
 	grid_container.columns = seedResource.get_size()
+	add_new_slot(seedResource.get_seed_list())
 	
+func add_new_slot(seedArray: Array[SeedData]) -> void:
+	for child in seedArray:
+		instance_slot(child)
+		
+func _on_slot_selected(value) -> void:
+	change_selected_slot(value)
+	
+func change_selected_slot(slot_pos) -> void:
+	selector_texture.position.x = slot_pos.x
+
+func instance_slot(seedData: SeedData) -> void:
+	var slot = slotScene.instantiate()
+	grid_container.add_child(slot)
+	slot.connect("slot_selected", _on_slot_selected)
+	slot.setup(seedData)
