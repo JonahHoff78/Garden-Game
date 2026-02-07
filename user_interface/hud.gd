594b262ea0b6d40f8e4926df5779ff036ec9e56f
Jonah Hoff <jojoyo98@icloud.com>
2026-02-07 22:54:13 +0000

    Animation a1

--- user_interface/hud.gd
+++ user_interface/hud.gd
@@ -1,5 +1,25 @@
 extends CanvasLayer
 
+@onready var flourist_shop = $FlouristShop
+@onready var inventory = $Inventory
+
+
+@onready var screenSize = get_viewport().get_visible_rect().size
+
+var inventory_hidden: bool = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggleInventory") and not flourist_shop.visible:
+		inventory_hidden = not inventory_hidden
+		toggle_inventory_ui()
+		
+func toggle_inventory_ui() -> void:
+	create_tween().set_ease(Tween.EASE_IN_OUT).tween_property(
+		inventory, "position:y",
+		screenSize.y + inventory.size.y if inventory_hidden else screenSize.y - inventory.size.y, 
+		0.5
+	)
+
 func setup_inventory() -> void:
 	$Inventory.initialize()
 	
